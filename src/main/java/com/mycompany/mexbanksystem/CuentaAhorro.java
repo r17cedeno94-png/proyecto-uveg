82e2289ad9a277dd00a4c5a3d7a1d735590e521b
r17cedeno94-png <r17cedeno94@gmail.com>
2026-03-04 22:34:47 -0600

    Subiendo Hola A Todos

--- src/main/java/com/mycompany/mexbanksystem/CuentaAhorro.java
+++ src/main/java/com/mycompany/mexbanksystem/CuentaAhorro.java
@@ -10,6 +10,8 @@ public class CuentaAhorro extends CuentaBase {
         double ganancia = cantidad + 0.10;
         montoActual += ganancia;
         System.out.print("Inversion del 10% aplicada. nuevo monto: $" + montoActual);
+        System.out.print("Hola A Todos");
     }
+    
     
 }
